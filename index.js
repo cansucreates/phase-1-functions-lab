4a622e5ad95dd7722a5ace35a984aa60bd6d79e4
Cansu Aysagdic <cansu.aysagdic@ogr.iu.edu.tr>
2023-03-19 23:42:43 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLoc) {
+const scuberHq = 42;
+  
+if (pickupLoc <= scuberHq) {
+return scuberHq - pickupLoc;
+}
+else {
+return pickupLoc - scuberHq;
+}
+}
+  
+function distanceFromHqInFeet(pickupLoc) {
+const blockLength = 264;
+const distanceInBlocks = distanceFromHqInBlocks(pickupLoc);
+return distanceInBlocks * blockLength;
+}
+  
+function distanceTravelledInFeet(start, destination) {
+const blockLength = 264;
+const distanceInBlocks = Math.abs(start - destination);
+return distanceInBlocks * blockLength;
+}
+  
+function calculatesFarePrice(start, destination) {
+const distance = distanceTravelledInFeet(start, destination);
+let fare;
+  
+if (distance <= 400) {
+    fare = 0;
+} 
+else if (distance <= 2000) {
+    fare = (distance - 400) * 0.02;
+} 
+else if (distance <= 2500) {
+    fare = 25;
+} 
+else {
+return "cannot travel that far";
+}
+  
+return fare;
+}
